8f60ecf6600922e6b7f262e5f94ee91facff0a9c
ayaanlee-igaw <ayaan.lee@igaworks.com>
2023-10-30 11:50:59 +0900

    feat: select, input append and prepend

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -94,7 +94,7 @@
 
     <div>
       <h2>Input</h2>
-      <div>
+      <div style="display: flex; flex-direction: column; gap: 10px">
         <el-input placeholder="Input" />
         <el-input class="el-input--medium" placeholder="Input" />
         <el-input class="el-input--small" placeholder="Input" />
@@ -102,7 +102,7 @@
         <el-input placeholder="Input" disabled/>
       </div>
 
-      <div>
+      <div style="display: flex; flex-direction: column; gap: 10px">
         <el-input v-model="input1" placeholder="Please input">
           <template #prepend>Http://</template>
         </el-input>
@@ -110,9 +110,9 @@
           <template #append>.com</template>
         </el-input>
         <el-input
-            v-model="input3"
-            placeholder="Please input"
-            class="input-with-select">
+          v-model="input3"
+          placeholder="Please input"
+          class="input-with-select">
           <template #prepend>
             <el-select v-model="select" placeholder="Select" style="width: 115px">
               <el-option label="Restaurant" value="1" />
@@ -125,9 +125,9 @@
           </template>
         </el-input>
         <el-input
-            v-model="input3"
-            placeholder="Please input"
-            class="input-with-select">
+          v-model="input3"
+          placeholder="Please input"
+          class="input-with-select">
           <template #prepend>
             <el-button :icon="Search" />
           </template>
@@ -141,98 +141,164 @@
         </el-input>
       </div>
 
-      <div class="demo-input-size">
+      <div class="demo-input-size" style="display: flex; flex-direction: column; gap: 10px">
         <el-input
-            v-model="input2"
-            placeholder="Please Input"
-            :suffix-icon="Search"
-        />
+          v-model="input2"
+          placeholder="Please Input"
+          :suffix-icon="Search" />
         <el-input
-            v-model="input3"
-            placeholder="Please Input"
-            :suffix-icon="Search"
-        />
+          v-model="input3"
+          placeholder="Please Input"
+          :suffix-icon="Search" />
       </div>
       <div class="demo-input-size">
         <el-input
-            v-model="input1"
-            placeholder="Please Input"
-            :prefix-icon="Search"
-        />
+          v-model="input1"
+          placeholder="Please Input"
+          :prefix-icon="Search" />
         <el-input v-model="input1" placeholder="Please Input" maxlength="10" show-word-limit clearable/>
       </div>
 
       <div>
         <el-input
-            v-model="textarea1"
-            autosize
-            type="textarea"
-            placeholder="Please input"
-        />
+          v-model="textarea1"
+          autosize
+          type="textarea"
+          placeholder="Please input" />
         <el-input
-            v-model="textarea2"
-            type="textarea"
-            placeholder="Please input"
-            disabled
-        />
+          v-model="textarea2"
+          type="textarea"
+          placeholder="Please input"
+          disabled />
       </div>
 
       <div>
         <el-input-number v-model="num" :min="1" :max="10" @change="handleChange" />
         <el-input-number
-            v-model="num"
-            :min="1"
-            :max="10"
-            controls-position="right"
-            @change="handleChange"
-        />
+          v-model="num"
+          :min="1"
+          :max="10"
+          controls-position="right"
+          @change="handleChange" />
         <el-input-number v-model="num" :min="1" :max="10" @change="handleChange" disabled/>
         <el-input-number
-            v-model="num"
-            :min="1"
-            :max="10"
-            controls-position="right"
-            @change="handleChange"
-            disabled
-        />
+          v-model="num"
+          :min="1"
+          :max="10"
+          controls-position="right"
+          @change="handleChange"
+          disabled />
       </div>
 
     </div>
 
     <div>
       <h2>Select</h2>
-      <el-select v-model="value" class="el-select--large" placeholder="Select" disabled>
-        <el-option
+
+      <div>
+        <el-select v-model="value" class="el-select--large" placeholder="Select" disabled>
+          <el-option
             v-for="item in options"
             :key="item.value"
             :label="item.label"
-            :value="item.value"
-        />
-      </el-select>
-      <el-select v-model="value" class="el-select--medium" placeholder="Select">
-        <el-option
-            v-for="item in options"
+            :value="item.value" />
+        </el-select>
+        <el-select v-model="value" class="el-select--medium" placeholder="Select" clearable filterable>
+          <el-option
+            v-for="(item, index) in options"
             :key="item.value"
             :label="item.label"
             :value="item.value"
-        />
-      </el-select>
-      <el-select v-model="value" class="el-select--small" placeholder="Select">
-        <el-option
+            :disabled="index === 0" />
+        </el-select>
+        <el-select v-model="value" class="el-select--small" placeholder="Select">
+          <el-option
             v-for="item in options"
             :key="item.value"
             :label="item.label"
-            :value="item.value"
-        />
-      </el-select>
-      <el-select v-model="value" class="el-select--mini" placeholder="Select">
-        <el-option
+            :value="item.value" />
+        </el-select>
+        <el-select v-model="value" class="el-select--mini" placeholder="Select">
+          <el-option
             v-for="item in options"
             :key="item.value"
             :label="item.label"
-            :value="item.value"
-        />
-      </el-select>
+            :value="item.value" />
+        </el-select>
+      </div>
+
+      <div>
+        <el-select
+          v-model="value1"
+          class="el-select--large"
+          multiple
+          filterable
+          placeholder="Select"
+          style="width: 240px">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value" />
+        </el-select>
+        <el-select
+          v-model="value1"
+          class="el-select--medium"
+          multiple
+          collapse-tags
+          placeholder="Select"
+          style="width: 240px">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value" />
+        </el-select>
+        <el-select
+          v-model="value1"
+          class="el-select--small"
+          multiple
+          collapse-tags
+          collapse-tags-tooltip
+          placeholder="Select"
+          style="width: 240px">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value" />
+        </el-select>
+        <el-select
+          v-model="value1"
+          class="el-select--mini"
+          multiple
+          collapse-tags
+          collapse-tags-tooltip
+          :max-collapse-tags="3"
+          placeholder="Select"
+          style="width: 240px">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value" />
+        </el-select>
+      </div>
+
+      <div>
+        <el-select v-model="value3" placeholder="Select">
+          <el-option-group
+            v-for="group in options3"
+            :key="group.label"
+            :label="group.label">
+            <el-option
+              v-for="item in group.options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value" />
+          </el-option-group>
+        </el-select>
+      </div>
     </div>
   </div>
 </template>
@@ -253,6 +319,8 @@ const handleChange = (value: number) => {
 }
 
 const value = ref('')
+const value1 = ref([])
+const value3 = ref('')
 
 const options = [
   {
@@ -274,6 +342,43 @@ const options = [
   {
     value: 'Option5',
     label: 'Option5',
+  },
+]
+
+const options3 = [
+  {
+    label: 'Popular cities',
+    options: [
+      {
+        value: 'Shanghai',
+        label: 'Shanghai',
+      },
+      {
+        value: 'Beijing',
+        label: 'Beijing',
+      },
+    ],
+  },
+  {
+    label: 'City name',
+    options: [
+      {
+        value: 'Chengdu',
+        label: 'Chengdu',
+      },
+      {
+        value: 'Shenzhen',
+        label: 'Shenzhen',
+      },
+      {
+        value: 'Guangzhou',
+        label: 'Guangzhou',
+      },
+      {
+        value: 'Dalian',
+        label: 'Dalian',
+      },
+    ],
   },
 ]
 </script>
