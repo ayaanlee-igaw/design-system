82ddad31fb6f84cf6b189b8b4353cb3da853fd0f
ayaanlee-igaw <ayaan.lee@igaworks.com>
2023-10-30 15:12:42 +0900

    fix: fragment

--- src/components/CheckBox/CheckBox.vue
+++ src/components/CheckBox/CheckBox.vue
@@ -1,34 +1,29 @@
 <template>
+  <h2>Check Box</h2>
   <div>
-    <h2>Check Box</h2>
-
-    <div>
-      <el-checkbox v-model="checked1" label="Option 1" />
-      <el-checkbox v-model="checked1" label="Option 1" disabled/>
-    </div>
-
-    <div>
-      <el-checkbox
-        v-model="checkAll"
-        :indeterminate="isIndeterminate"
-        @change="handleCheckAllChange">
-        Check all
+    <el-checkbox v-model="checked1" label="Option 1" />
+    <el-checkbox v-model="checked1" label="Option 1" disabled/>
+  </div>
+  <div>
+    <el-checkbox
+      v-model="checkAll"
+      :indeterminate="isIndeterminate"
+      @change="handleCheckAllChange">
+      Check all
+    </el-checkbox>
+    <el-checkbox-group
+      v-model="checkedCities"
+      @change="handleCheckedCitiesChange">
+      <el-checkbox v-for="city in cities" :key="city" :label="city">
+        {{ city }}
       </el-checkbox>
-      <el-checkbox-group
-        v-model="checkedCities"
-        @change="handleCheckedCitiesChange">
-        <el-checkbox v-for="city in cities" :key="city" :label="city">
-          {{ city }}
-        </el-checkbox>
-      </el-checkbox-group>
-    </div>
-
-    <div>
-      <el-checkbox v-model="checked1" label="Option1" class="el-checkbox--large" border />
-      <el-checkbox v-model="checked2" label="Option2" class="el-checkbox--medium" border />
-      <el-checkbox v-model="checked1" label="Option1" class="el-checkbox--small" border />
-      <el-checkbox v-model="checked2" label="Option2" class="el-checkbox--mini" border />
-    </div>
+    </el-checkbox-group>
+  </div>
+  <div>
+    <el-checkbox v-model="checked1" label="Option1" class="el-checkbox--large" border />
+    <el-checkbox v-model="checked2" label="Option2" class="el-checkbox--medium" border />
+    <el-checkbox v-model="checked1" label="Option1" class="el-checkbox--small" border />
+    <el-checkbox v-model="checked2" label="Option2" class="el-checkbox--mini" border />
   </div>
 </template>
 
--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,104 +1,101 @@
 <template>
+  <h2>Input</h2>
+  <div style="display: flex; flex-direction: column; gap: 10px">
+    <el-input placeholder="Input" />
+    <el-input class="el-input--medium" placeholder="Input" />
+    <el-input class="el-input--small" placeholder="Input" />
+    <el-input class="el-input--mini" placeholder="Input" />
+    <el-input placeholder="Input" disabled/>
+  </div>
+
+  <div style="display: flex; flex-direction: column; gap: 10px">
+    <el-input v-model="input1" placeholder="Please input">
+      <template #prepend>Http://</template>
+    </el-input>
+    <el-input v-model="input2" placeholder="Please input">
+      <template #append>.com</template>
+    </el-input>
+    <el-input
+        v-model="input3"
+        placeholder="Please input"
+        class="input-with-select">
+      <template #prepend>
+        <el-select v-model="select" placeholder="Select" style="width: 115px">
+          <el-option label="Restaurant" value="1" />
+          <el-option label="Order No." value="2" />
+          <el-option label="Tel" value="3" />
+        </el-select>
+      </template>
+      <template #append>
+        <el-button :icon="Search" />
+      </template>
+    </el-input>
+    <el-input
+        v-model="input3"
+        placeholder="Please input"
+        class="input-with-select">
+      <template #prepend>
+        <el-button :icon="Search" />
+      </template>
+      <template #append>
+        <el-select v-model="select" placeholder="Select" style="width: 115px">
+          <el-option label="Restaurant" value="1" />
+          <el-option label="Order No." value="2" />
+          <el-option label="Tel" value="3" />
+        </el-select>
+      </template>
+    </el-input>
+  </div>
+
+  <div class="demo-input-size" style="display: flex; flex-direction: column; gap: 10px">
+    <el-input
+        v-model="input2"
+        placeholder="Please Input"
+        :suffix-icon="Search" />
+    <el-input
+        v-model="input3"
+        placeholder="Please Input"
+        :suffix-icon="Search" />
+  </div>
+  <div class="demo-input-size">
+    <el-input
+        v-model="input1"
+        placeholder="Please Input"
+        :prefix-icon="Search" />
+    <el-input v-model="input1" placeholder="Please Input" maxlength="10" show-word-limit clearable/>
+  </div>
+
   <div>
-    <h2>Input</h2>
-    <div style="display: flex; flex-direction: column; gap: 10px">
-      <el-input placeholder="Input" />
-      <el-input class="el-input--medium" placeholder="Input" />
-      <el-input class="el-input--small" placeholder="Input" />
-      <el-input class="el-input--mini" placeholder="Input" />
-      <el-input placeholder="Input" disabled/>
-    </div>
+    <el-input
+        v-model="textarea1"
+        autosize
+        show-word-limit
+        maxlength="9"
+        type="textarea"
+        placeholder="Please input" />
+    <el-input
+        v-model="textarea2"
+        type="textarea"
+        placeholder="Please input"
+        disabled />
+  </div>
 
-    <div style="display: flex; flex-direction: column; gap: 10px">
-      <el-input v-model="input1" placeholder="Please input">
-        <template #prepend>Http://</template>
-      </el-input>
-      <el-input v-model="input2" placeholder="Please input">
-        <template #append>.com</template>
-      </el-input>
-      <el-input
-          v-model="input3"
-          placeholder="Please input"
-          class="input-with-select">
-        <template #prepend>
-          <el-select v-model="select" placeholder="Select" style="width: 115px">
-            <el-option label="Restaurant" value="1" />
-            <el-option label="Order No." value="2" />
-            <el-option label="Tel" value="3" />
-          </el-select>
-        </template>
-        <template #append>
-          <el-button :icon="Search" />
-        </template>
-      </el-input>
-      <el-input
-          v-model="input3"
-          placeholder="Please input"
-          class="input-with-select">
-        <template #prepend>
-          <el-button :icon="Search" />
-        </template>
-        <template #append>
-          <el-select v-model="select" placeholder="Select" style="width: 115px">
-            <el-option label="Restaurant" value="1" />
-            <el-option label="Order No." value="2" />
-            <el-option label="Tel" value="3" />
-          </el-select>
-        </template>
-      </el-input>
-    </div>
-
-    <div class="demo-input-size" style="display: flex; flex-direction: column; gap: 10px">
-      <el-input
-          v-model="input2"
-          placeholder="Please Input"
-          :suffix-icon="Search" />
-      <el-input
-          v-model="input3"
-          placeholder="Please Input"
-          :suffix-icon="Search" />
-    </div>
-    <div class="demo-input-size">
-      <el-input
-          v-model="input1"
-          placeholder="Please Input"
-          :prefix-icon="Search" />
-      <el-input v-model="input1" placeholder="Please Input" maxlength="10" show-word-limit clearable/>
-    </div>
-
-    <div>
-      <el-input
-          v-model="textarea1"
-          autosize
-          show-word-limit
-          maxlength="9"
-          type="textarea"
-          placeholder="Please input" />
-      <el-input
-          v-model="textarea2"
-          type="textarea"
-          placeholder="Please input"
-          disabled />
-    </div>
-
-    <div>
-      <el-input-number v-model="num" :min="1" :max="10" @change="handleChange" />
-      <el-input-number
-          v-model="num"
-          :min="1"
-          :max="10"
-          controls-position="right"
-          @change="handleChange" />
-      <el-input-number v-model="num" :min="1" :max="10" @change="handleChange" disabled/>
-      <el-input-number
-          v-model="num"
-          :min="1"
-          :max="10"
-          controls-position="right"
-          @change="handleChange"
-          disabled />
-    </div>
-
+  <div>
+    <el-input-number v-model="num" :min="1" :max="10" @change="handleChange" />
+    <el-input-number
+        v-model="num"
+        :min="1"
+        :max="10"
+        controls-position="right"
+        @change="handleChange" />
+    <el-input-number v-model="num" :min="1" :max="10" @change="handleChange" disabled/>
+    <el-input-number
+        v-model="num"
+        :min="1"
+        :max="10"
+        controls-position="right"
+        @change="handleChange"
+        disabled />
   </div>
 </template>
 
