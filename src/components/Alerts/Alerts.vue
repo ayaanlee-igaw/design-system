429cbb0c9dd4722b752b8d3c9a105666fe7bf4d0
ayaanlee-igaw <ayaan.lee@igaworks.com>
2023-11-03 09:54:46 +0900

    fix: alert

--- src/components/Alerts/Alerts.vue
+++ src/components/Alerts/Alerts.vue
@@ -1,14 +1,18 @@
 <template>
-  <Alert :type="AlertType.success">
-    <template v-slot:default>
-      <div>slot binding</div>
-    </template>
-  </Alert>
-  <Alert :type="AlertType.warning" title="Warning" buttonLabel="Button" :handleButton="handleButton" description="title, button, center" center />
-  <Alert :type="AlertType.warning" buttonLabel="Button" :handleButton="handleButton" description="button and center" center />
-  <Alert :type="AlertType.info" title="Info" description="title and center" center/>
-  <Alert :type="AlertType.danger" description="danger" center />
-  <Alert :type="AlertType.danger" title="danger" center />
+  <h2>Alert</h2>
+
+  <div class="alert-wrap">
+    <Alert :type="AlertType.success">
+      <template v-slot:default>
+        <div>slot binding</div>
+      </template>
+    </Alert>
+    <Alert :type="AlertType.warning" title="Warning" buttonLabel="Button" :handleButton="handleButton" description="title, button, center" center />
+    <Alert :type="AlertType.warning" buttonLabel="Button" :handleButton="handleButton" description="button and center" center />
+    <Alert :type="AlertType.info" title="Info" description="title and center" center/>
+    <Alert :type="AlertType.danger" description="danger" center />
+    <Alert :type="AlertType.danger" title="danger" center />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -21,4 +25,9 @@ function handleButton () {
 </script>
 
 <style scoped>
+.alert-wrap {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
 </style>
